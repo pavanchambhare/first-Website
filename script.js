527075f347622d2f46a72827bf286f20e678d21a
pavanchambhare <pavanchambhare88gmail.com>
2022-04-16 17:33:11 +0530

    first commit

--- script.js
+++ script.js
@@ -6,12 +6,43 @@ const toggleNavbar = () => {
 };
 mobile_nav.addEventListener("click", () => toggleNavbar());
 
-  
-var pics = document.getElementById("pic")
-var gallery = document.getElementById("gallery")
-
-function openGallery(pics){
- pics.style.display = "flex";
- gallery.src = pics;
-
+ if (inputImage.complete) {
+  flipImage();
 }
+else {
+  inputImage.onload = flipImage;
+}
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+window.scrollBy({ 
+  top: 100,
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+document.querySelector('.hello').scrollIntoView({ 
+  behavior: 'smooth' 
+});
